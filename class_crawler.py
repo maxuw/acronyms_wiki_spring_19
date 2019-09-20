283a17718889e405a8a43b287d14531af4bc5ed9
Max <m.jackl@student.uw.edu.pl>
2019-09-21 01:52:25 +0200

    moved all function to the class_crawler

--- class_crawler.py
+++ class_crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
+import pandas as pd
 
 class Crawler:
 
@@ -20,4 +21,97 @@ class Crawler:
             final_list.append(lines)
 
         return final_list
+
+
+    def readAll(self, nested_links, acronyms_data_frame, df_index, verbose):
+        for list_link in nested_links:
+
+            for link in list_link:
+                link = link[:-1]
+                paragraph, wiki_end = self.getParagraph(link, verbose)
+                series_acronym = self.getAcronymFromParapraph(paragraph, wiki_end, df_index, verbose)
+                if verbose: print(series_acronym)
+
+                acronyms_data_frame = acronyms_data_frame.append(series_acronym, ignore_index=True)
+
+        return acronyms_data_frame
+
+
+    def getParagraph(self, url, verbose):
+
+        page = urllib.request.urlopen(url)
+
+        soup = BeautifulSoup(page, 'html.parser')
+
+        wikiEnd = re.search(r'\/wiki\/.+', url)
+        wikiEnd = wikiEnd.group()[6:]
+
+        header_section = soup.find_all('p')
+
+        if len(header_section[0]) < 6:
+            header_section = header_section[1]
+
+        else:
+            header_section = header_section[0]
+
+        paragraph = header_section.getText()
+
+        return paragraph, wikiEnd
+
+
+    def getAcronymFromParapraph(self, paragraph, wiki_end, df_index, verbose):
+
+    #     print(text_work)
+        matchObjExtension = re.match(r'(\w|\s)+', paragraph, re.UNICODE)
+
+        matchObjExtension = matchObjExtension.group()
+
+        if matchObjExtension[-1] == " ":
+            # print("space detected")
+
+            matchObjExtension = matchObjExtension[0:-1]
+
+
+
+        matchObjAcronym = re.search(r'[A-Z][A-Z]+', paragraph) # ogranicz do pierwszego akapitu.
+
+        if matchObjAcronym == None:
+            matchObjAcronym = "brak"
+
+        else:
+            matchObjAcronym = matchObjAcronym.group()
+
+        matchObjTranslation = re.search(r'\(\w[a-z][a-z]..(\w|\s)+', paragraph)
+
+    #     if matchObjTranslation == None:
+    #         matchObjTranslation = re.search(r'\((\w|\s)+', paragraph)
+    # #     matchObjTranslation = re.search(r'\w\w\w.\xa0(\w|\s)+', text_work)
+
+        if matchObjTranslation == None:
+
+            print("no translation")
+            matchObjLanguage = "brak"
+            matchObjTranslationProper = "brak"
+
+
+        else:
+    #         print("dupa")
+    #         print(matchObjTranslation)
+
+            matchObjLanguage = re.search(r'\w+', matchObjTranslation.group())
+    #         print(matchObjLanguage)
+            matchObjLanguage = matchObjLanguage.group()
+            matchObjTranslationProper = re.search(r'\s(\w|\s)+', matchObjTranslation.group())
+
+    #         print(matchObjTranslationProper)
+            if matchObjTranslationProper == None:
+                matchObjTranslationProper = "brak"
+            else:
+                matchObjTranslationProper = matchObjTranslationProper.group()[1:]
+
+        series_acronym = pd.Series([matchObjAcronym, matchObjExtension,
+                                    matchObjTranslationProper, matchObjLanguage, wiki_end],
+                                   index=df_index)
+        return series_acronym
+
 
