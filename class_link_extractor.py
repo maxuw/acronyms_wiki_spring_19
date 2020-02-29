f7f998e4737a712e6bd3b9ae4cd2886422ef6549
Max <m.jackl@student.uw.edu.pl>
2020-03-01 00:21:26 +0100

    added a type_page function that categorizes wikipedia pages

--- class_link_extractor.py
+++ class_link_extractor.py
@@ -1,1 +1,42 @@
 # link extractor class
+
+
+def type_page(url):
+
+    if "pl.wikipedia.org" not in url:
+
+        return "not a polish wikipedia page"
+
+    if "https://" in url:
+        url = url.replace("https://", "")
+
+    elif "http://" in url:
+        url = url.replace("http://", "")
+
+    if "www." in url:
+        url = url.replace("www.", "")
+
+    url = url.replace("pl.wikipedia.org/", "")
+
+    if "wiki/" not in url:
+
+        return "wikipedia, but not an encyclopedia type of page"
+
+    else:
+        url = url.replace("wiki/", "")
+
+        print(url)
+
+        if "Kategoria:" in url:
+
+            return "category"
+
+        elif "Portal:" in url:
+
+            return "portal_page"
+
+        elif ":" not in url:
+            return "record_page"
+
+        else:
+            return "unknown type of page"
